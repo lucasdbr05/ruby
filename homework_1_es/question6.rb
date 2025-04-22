488b81d68f826c083d6ffda888c7d2feabb111e1
Lucas Lima <lucasgabrieljake@gmail.com>
2025-04-22 08:09:36 -0300

    minor changes

--- homework_1_es/question6.rb
+++ homework_1_es/question6.rb
@@ -14,7 +14,7 @@ class Numeric
   @@currencies = {'dollar'=> 1, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
 
   def method_missing(method_id, *args, &block) 
-    singular_currency = method_id.to_s.gsub( /s$/, '')
+    singular_currency = method_id.to_s.gsub(/s$/, '')
     if @@currencies.has_key?(singular_currency)
       self * @@currencies[singular_currency]
     else
